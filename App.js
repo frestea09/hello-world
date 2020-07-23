af38e334ea7433fe64fdf2ee86ab3dce52211436
DESKTOP-OL6NMDJ\ASUS <ilmanfrasetya@gmail.com>
2020-07-22 22:00:08 -0700

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, PickerIOSComponent } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.header}>
+        <Text style={styles.boldText}>Hello, Worlds</Text>
+      </View>
+      <Text>Hello World</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -18,4 +21,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  header : {
+    backgroundColor : 'pink',
+    padding:20,
+  },
+  boldText:{
+    fontWeight:'bold'
+  }
 });
